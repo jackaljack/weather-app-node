c7c2001e1671214acc505fea2acb9ddb704a6bc2
jackaljack <jackdebidda@gmail.com>
2017-04-22 15:17:05 +0200

    Add simple https request

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('Starting node app...');
+
+const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View',
+  json: true,
+}, (error, response, body) => {
+  // console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+});
